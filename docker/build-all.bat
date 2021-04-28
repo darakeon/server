e72c4bf9149293715553e65ac24fc3ad6c8ee08c
Dara Keon <lc.sales.duarte@gmail.com>
2021-04-28 19:18:45 -0300

    chore: refactor build

--- docker/build-all.bat
+++ docker/build-all.bat
@@ -1,13 +1,7 @@
 @echo off
 
-set dir=%~dp0
 set push=%~1
 
-call single-build.bat ubuntu %push%
-call single-build.bat netcore %push%
-
-docker build .. -t darakeon/server -f "%dir%server.dockerfile"
-
-if "%push%" neq "" (
-	docker %push% darakeon/server
-)
+call single-build.bat ubuntu . %push%
+call single-build.bat netcore . %push%
+call single-build.bat server .. %push%
