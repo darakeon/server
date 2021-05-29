45c65ba5d0d5e29426daa74d3e2ee71c54f93966
Dara Keon <lc.sales.duarte@gmail.com>
2021-05-29 19:00:14 -0300

    docker: remove removal parameter
    Robot is killing sites at server when using this parameter

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -3,4 +3,4 @@
 docker-compose -f ../docker/compose/$1.yml pull --ignore-pull-failures
 
 export TASK=$2
-docker-compose -f ../docker/compose/$1.yml up -d --remove-orphans
+docker-compose -f ../docker/compose/$1.yml up -d
