7f335ed78e4ab44875deec6098b8d448c3872653
Dara Keon <lc.sales.duarte@gmail.com>
2021-06-03 20:55:16 -0300

    robot: separate compose of schedules and wipe
    They were using same logs folder and overwritten one each other when
    machines up

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 PROJ=$1
-export TASK=$2
 
-./print-date.sh start compose $PROJ $TASK
+./print-date.sh start compose $PROJ
 
 docker-compose -p $PROJ -f ../docker/compose/$PROJ.yml pull --ignore-pull-failures
 docker-compose -p $PROJ -f ../docker/compose/$PROJ.yml up -d --remove-orphans
 
-./print-date.sh end compose $PROJ $TASK
+./print-date.sh end compose $PROJ
