00d3816e75f9328d0d5afc76b1875cc144f71bdb
Dara Keon <lc.sales.duarte@gmail.com>
2022-03-31 13:30:51 -0300

    docker: move scripts from bat to sh
    Docker Desktop is now paid. Now using docker at windows is throught WSL2

---
+++ docker/build/single.sh
@@ -0,0 +1,20 @@
+DIR=$(dirname $0)
+MACHINE=$1
+
+if [ "$MACHINE" == "" ]; then
+	echo "missing machine name"
+	exit 1
+fi
+
+CONTEXT=$2
+PUSH=$3
+
+if [ "$CONTEXT" == "" ]; then
+	CONTEXT=$DIR
+fi
+
+docker build $CONTEXT --pull -f $DIR/../images/$MACHINE.dockerfile -t darakeon/$MACHINE --network=host
+
+if [ "$PUSH" == "push" ]; then
+	docker push darakeon/$MACHINE
+fi
