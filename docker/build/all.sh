1f686412587f6691eb89a32c87e3e7bbaa56e141
Dara Keon <lc.sales.duarte@gmail.com>
2022-06-02 21:36:29 -0300

    ci: split docker build+push steps

--- docker/build/all.sh
+++ docker/build/all.sh
@@ -4,8 +4,3 @@ PUSH=$1
 ./single.sh ubuntu . $PUSH
 ./single.sh netcore . $PUSH
 ./single.sh server ../.. $PUSH
-
-./single.sh talisman . $PUSH
-
-# ./single.sh aws . $PUSH
-# ./single.sh aws-terraform . $PUSH
