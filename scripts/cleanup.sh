b6fa51fb3dd491f6c85de40941806ddfe498ed05
Dara Keon <lc.sales.duarte@gmail.com>
2024-04-04 12:57:07 +0100

    docker: remove total cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,10 +6,10 @@ echo BEFORE
 df --output=source,size,pcent -h | grep /dev/root
 echo --------------------------------------------------------------------------------
 
-docker container prune -f > /dev/null
+#docker container prune -f > /dev/null
 docker image prune -f -a > /dev/null
 docker network prune -f > /dev/null
-docker system prune -f > /dev/null
+#docker system prune -f > /dev/null
 
 echo --------------------------------------------------------------------------------
 date
