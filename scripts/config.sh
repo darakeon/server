2272a5f2111a2df5b068f2c47039a5eb26eddbe1
Dara Keon <lc.sales.duarte@gmail.com>
2024-07-27 22:52:43 +0100

    bootstrap: add tip of which file person is editting

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,20 +7,20 @@ cd cfg
 
 mkdir dfm
 cd dfm
-nano db.amazon.json
-nano smtp.amazon.json
-nano s3.amazon.json
-nano sqs.amazon.json
-nano login.amazon.json
+echo $PWD/db.amazon.json > db.amazon.json && nano db.amazon.json
+echo $PWD/smtp.amazon.json > smtp.amazon.json && nano smtp.amazon.json
+echo $PWD/s3.amazon.json > s3.amazon.json && nano s3.amazon.json
+echo $PWD/sqs.amazon.json > sqs.amazon.json && nano sqs.amazon.json
+echo $PWD/login.amazon.json > login.amazon.json && nano login.amazon.json
 cd ..
 
 mkdir sql-sunner
 sql-sunner
-nano prod.env
-nano prod.py
+echo $PWD/prod.env > prod.env && nano prod.env
+echo $PWD/prod.py > prod.py && nano prod.py
 cd ..
 
 mkdir midna
 cd midna
-nano .env
+echo $PWD/.env > .env && nano .env
 cd ..
