cb3c02b7838e20eff95516a0fa4e0b4f09a95f53
Dara Keon <lc.sales.duarte@gmail.com>
2024-07-27 22:40:21 +0100

    bootstrap: add config files

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,10 +2,14 @@
 
 cd ~
 git clone https://github.com/darakeon/server.git
-cd server
 
+cd ~/server/scripts
 ./docker.sh
 
 echo 'export PS1="\n\n[\[\033[01;30m\]\A\[\033[00m\]] 🐳 \$(docker ps -q | wc -l)/\$(docker ps -a -q | wc -l) \[\033[01;31m\]\u\[\033[00m\]@\[\033[01;35m\]\`if [[ \$PWD = /mnt/c* ]]; then echo \"#\"; else echo \"&\"; fi\`/\W\[\033[00m\]$ "' > ~/.bashrc
+
+cd ~/server/scripts
+
+./config.sh
 
 ./upgrade.sh
---
+++ scripts/config.sh
@@ -0,0 +1,19 @@
+cd /var
+sudo mkdir cfg && cd cfg
+
+mkdir dfm && cd dfm
+nano db.amazon.json
+nano smtp.amazon.json
+nano s3.amazon.json
+nano sqs.amazon.json
+nano login.amazon.json
+cd ..
+
+mkdir sql-sunner && sql-sunner
+nano prod.env
+nano prod.py
+cd ..
+
+mkdir midna && cd midna
+nano .env
+cd ..
