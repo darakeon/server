8ff0317b27e160996c1f94dd44bf1ea75a127035
Dara Keon <lc.sales.duarte@gmail.com>
2024-07-27 22:07:34 +0100

    docker: update compose command

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -4,7 +4,7 @@ PROJ=$1
 
 ./print-date.sh start compose $PROJ
 
-docker-compose -p $PROJ -f ../docker/compose/$PROJ.yml pull --ignore-pull-failures
-docker-compose -p $PROJ -f ../docker/compose/$PROJ.yml up -d --remove-orphans
+docker compose -p $PROJ -f ../docker/compose/$PROJ.yml pull --ignore-pull-failures
+docker compose -p $PROJ -f ../docker/compose/$PROJ.yml up -d --remove-orphans
 
 ./print-date.sh end compose $PROJ
--- scripts/decompose.sh
+++ scripts/decompose.sh
@@ -4,6 +4,6 @@ PROJ=$1
 
 ./print-date.sh start decompose $PROJ
 
-docker-compose -p $PROJ -f ../docker/compose/$PROJ.yml down
+docker compose -p $PROJ -f ../docker/compose/$PROJ.yml down
 
 ./print-date.sh end decompose $PROJ
