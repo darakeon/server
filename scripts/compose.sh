49ca223a5c03ff15c7c07d7cb08fedbd4558bf66
Dara Keon <lc.sales.duarte@gmail.com>
2024-08-31 23:29:27 +0100

    docker: decompose in case of compose fail

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -7,4 +7,10 @@ PROJ=$1
 docker compose -p $PROJ -f ../docker/compose/$PROJ.yml pull --ignore-pull-failures
 docker compose -p $PROJ -f ../docker/compose/$PROJ.yml up -d --remove-orphans
 
+UP_RESULT=$?
+
 ./print-date.sh end compose $PROJ
+
+if [ $UP_RESULT != 0 ]; then
+    ./decompose.sh $PROJ
+fi
