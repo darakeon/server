fa1264e1b694ada24ba17ab589a56cb6f7ce1cff
Dara Keon <lc.sales.duarte@gmail.com>
2024-09-25 21:48:07 +0100

    cron: add running robot

---
+++ scripts/robot-run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+TASK=$1
+
+./print-date.sh start docker run robot for $TASK
+
+docker pull darakeon/dfm-robot
+
+docker run -it \
+	--name robot_$TASK \
+	-e TASK=$TASK \
+	-v /var/cfg/dfm:/var/cfg \
+	-v /var/data/dfm/robot-$TASK:/var/robot/data \
+	-v /var/data/dfm/nh-$TASK:/var/logs/dfm/nh \
+	--rm \
+	darakeon/dfm-robot
+
+RUN_RESULT=$?
+
+./print-date.sh end docker run robot for $TASK - $RUN_RESULT
