d7cc17dda80b2131dc9873f58f26c4b0153d2abe
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-05 21:39:06 +0100

    boostrap: configure aws

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -6,3 +6,22 @@ sudo apt-get install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+rm awscliv2.zip
+rm -r aws
+
+if [ ! -e ~/.aws ]; then
+    mkdir ~/.aws
+fi
+
+if [ ! -e ~/.aws/config ]; then
+    echo "[default]" > ~/.aws/config
+    echo "region = us-west-2" >> ~/.aws/config
+    echo "output = json" >> ~/.aws/config
+fi
+
+if [ ! -e ~/.aws/credentials ]; then \
+    echo "[default]" > ~/.aws/credentials; \
+    echo "aws_access_key_id = " >> ~/.aws/credentials; \
+    echo "aws_secret_access_key = " >> ~/.aws/credentials; \
+    nano ~/.aws/credentials; \
+fi
