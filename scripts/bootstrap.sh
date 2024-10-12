ed1dec3a2ce873d20fe8f34b030a6e0cb2c1373d
Dara Keon <lc.sales.duarte@gmail.com>
2024-10-12 22:35:19 +0100

    bootstrap: add make

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,6 +7,7 @@ git clone https://github.com/darakeon/server.git
 cd ~/server/scripts
 ./docker.sh
 ./aws.sh
+sudo apt install make
 
 echo 'export PS1="\n\n[\[\033[01;30m\]\A\[\033[00m\]] 🐳 \$(docker ps -q | wc -l)/\$(docker ps -a -q | wc -l) \[\033[01;31m\]\u\[\033[00m\]@\[\033[01;35m\]\`if [[ \$PWD = /mnt/c* ]]; then echo \"#\"; else echo \"&\"; fi\`/\W\[\033[00m\]$ "' >> ~/.bashrc
 
