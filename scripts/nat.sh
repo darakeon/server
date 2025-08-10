8428565003c13cdf70591b853c87ee27e7c51e36
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-10 23:30:01 +0100

    bootstrap: fix nat config - or I hope so

--- scripts/nat.sh
+++ scripts/nat.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set -e
 
-sysctl -w net.ipv4.ip_forward=1
 echo "# allow EC2 as NAT" >> /etc/sysctl.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
 
-export DEBIAN_FRONTEND=noninteractive
-INTERFACE=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="dev") print $(i+1)}')
-iptables -t nat -A POSTROUTING -o "$INTERFACE" -j MASQUERADE
-iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -j ACCEPT
+apt update
 apt install -y iptables-persistent
+
+iptables -t nat -A POSTROUTING -o eth0 -s 172.31.0.0/16 -j MASQUERADE
+
+# export DEBIAN_FRONTEND=noninteractive
+# INTERFACE=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="dev") print $(i+1)}')
+# iptables -t nat -A POSTROUTING -o "$INTERFACE" -j MASQUERADE
+# iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
+# iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -j ACCEPT
+
 netfilter-persistent save
+
+echo "*---------------------------------------------------------*"
+echo "|                                                         |"
+echo "|                                                         |"
+echo "|   REMEMBER disable source/destination check ON EC2!!!   |"
+echo "|                                                         |"
+echo "|                                                         |"
+echo "*---------------------------------------------------------*"
