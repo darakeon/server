c2d14f861d73fdc20537c6e60305c10b62e54232
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-11 00:09:48 +0100

    bootstrap: add some more commands for nat
    Some stuff that was in another file, it can be useful

--- scripts/nat.sh
+++ scripts/nat.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# original command
+# sudo sysctl -w net.ipv4.ip_forward=1
+
 echo "# allow EC2 as NAT" >> /etc/sysctl.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
@@ -13,8 +16,8 @@ iptables -t nat -A POSTROUTING -o eth0 -s 172.31.0.0/16 -j MASQUERADE
 # export DEBIAN_FRONTEND=noninteractive
 # INTERFACE=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="dev") print $(i+1)}')
 # iptables -t nat -A POSTROUTING -o "$INTERFACE" -j MASQUERADE
-# iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
-# iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -j ACCEPT
+# iptables -A FORWARD -i "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
+# iptables -A FORWARD -o "$INTERFACE" -j ACCEPT
 
 netfilter-persistent save
 
@@ -25,3 +28,6 @@ echo "|   REMEMBER disable source/destination check ON EC2!!!   |"
 echo "|                                                         |"
 echo "|                                                         |"
 echo "*---------------------------------------------------------*"
+
+# for check receiving:
+# sudo tcpdump -i ens5 icmp
