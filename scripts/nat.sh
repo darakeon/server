6cc5a1b757009c9bfc0bb6a25f5976a3ce0d7d75
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-10 14:40:53 +0100

    bootstrap: avoid interactive mode

--- scripts/nat.sh
+++ scripts/nat.sh
@@ -5,6 +5,7 @@ sysctl -w net.ipv4.ip_forward=1
 echo "# allow EC2 as NAT" >> /etc/sysctl.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
+export DEBIAN_FRONTEND=noninteractive
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
