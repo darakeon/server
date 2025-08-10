b422ae1cfea619bb70df618e9ad38fc30a862b70
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-10 14:51:50 +0100

    bootstrap: get right network interface

--- scripts/nat.sh
+++ scripts/nat.sh
@@ -6,8 +6,9 @@ echo "# allow EC2 as NAT" >> /etc/sysctl.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
 export DEBIAN_FRONTEND=noninteractive
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
+INTERFACE=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++) if ($i=="dev") print $(i+1)}')
+iptables -t nat -A POSTROUTING -o "$INTERFACE" -j MASQUERADE
+iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i "$INTERFACE" -o "$INTERFACE" -j ACCEPT
 apt install -y iptables-persistent
 netfilter-persistent save
