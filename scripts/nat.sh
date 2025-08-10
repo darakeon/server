d8bf1c45730f85b21ca467d493efd2a15f7953f0
Dara Keon <lc.sales.duarte@gmail.com>
2025-08-10 13:22:28 +0100

    bootstrap: complete EC2 as NAT config

--- scripts/nat.sh
+++ scripts/nat.sh
@@ -2,4 +2,11 @@
 set -e
 
 sysctl -w net.ipv4.ip_forward=1
+echo "# allow EC2 as NAT" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
+apt install -y iptables-persistent
+netfilter-persistent save
